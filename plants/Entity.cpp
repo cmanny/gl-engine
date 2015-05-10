784b9a1ffbda77edcb9ad512d630683f46ffb60a
nervousentity <ltucker_@live.co.uk>
2015-05-10 23:04:06 +0100

    Updated entity class

--- plants/Entity.cpp
+++ plants/Entity.cpp
@@ -14,3 +14,33 @@ Entity::~Entity() {
 VertexData* Entity::getData() {
   return data;
 }
+
+// Set x pos
+void Entity::setX(double x) {
+  this->x = x;
+}
+
+// Set y pos
+void Entity::setY(double y) {
+  this->y = y;
+}
+
+// Increment x pos
+void Entity::incX(double i) {
+  x += i;
+}
+
+// Increment y pos
+void Entity::incY(double i) {
+  y += i;
+}
+
+// Return x pos
+double Entity::getX() {
+  return x;
+}
+
+// Return y pos
+double Entity::getY() {
+  return y;
+}
--- plants/Entity.h
+++ plants/Entity.h
@@ -5,11 +5,20 @@
 class Entity {
   private:
     VertexData* data;
+    double x, y;
+
   public:
     Entity();
     ~Entity();
-    virtual void update(double delta()) {};
+    virtual void update(double delta) = 0;
     VertexData* getData();
+
+    void setX(double x);
+    void setY(double y);
+    void incX(double i);
+    void incY(double i);
+    double getX();
+    double getY();
 };
 
 #endif
